c18befe4c055e8caf437235f66c3ebce090580bb
Hugo Leclerc <hugal.leclerc@gmail.com>
2024-06-11 10:54:09 +0200

    long double

--- tests/cpp/test_PolyCon.cpp
+++ tests/cpp/test_PolyCon.cpp
@@ -81,51 +81,52 @@
 //     P( pc );
 // }
 
-// TEST_CASE( "PolyCon 3D", "" ) {
-//     constexpr int nb_dims = 3;
-//     using Scalar = double;
+TEST_CASE( "PolyCon 3D", "" ) {
+    constexpr int nb_dims = 3;
+    using Scalar = double;
 
-//     using Point = Vec<Scalar,nb_dims>;
+    using Point = Vec<Scalar,nb_dims>;
 
-//     Vec<Point> fun_dirs{ { 1., 0.1, 0.0 }, { 0.1, -0.7, 0.0 }, { 0., +0.7, 0. }, { 0., 0.0, +0.7 } };
-//     Vec<Scalar> fun_offs{ 0.0, 0.1, 0.2, 0.3 };
+    Vec<Point> fun_dirs{ { 1., 0.1, 0.0 }, { 0.1, -0.7, 0.0 }, { 0., +0.7, 0. }, { 0., 0.0, +0.7 } };
+    Vec<Scalar> fun_offs{ 0.0, 0.1, 0.2, 0.3 };
 
-//     // Vec<Point> bnd_dirs{
-//     //     { +1., 0., 0. }, { -1., 0., 0. },
-//     //     { 0., +1., 0. }, { 0., -1., 0. },
-//     //     { 0., 0., +1. }, { 0., 0., -1. }
-//     // };
-//     // Vec<Scalar> bnd_offs{ 5., 5., 5., 5., 5., 5. };
-//     Vec<Point> bnd_dirs{ { 1, 0, 0 } };
-//     Vec<Scalar> bnd_offs{ 5 };
+    // Vec<Point> bnd_dirs{
+    //     { +1., 0., 0. }, { -1., 0., 0. },
+    //     { 0., +1., 0. }, { 0., -1., 0. },
+    //     { 0., 0., +1. }, { 0., 0., -1. }
+    // };
+    // Vec<Scalar> bnd_offs{ 5., 5., 5., 5., 5., 5. };
+    Vec<Point> bnd_dirs{ { 1, 0, 0 } };
+    Vec<Scalar> bnd_offs{ 5 };
 
-//     PolyCon<Scalar,nb_dims> pa( fun_dirs, fun_offs, bnd_dirs, bnd_offs );
-//     pa.normalize();
-//     P( pa );
+    PolyCon<Scalar,nb_dims> pa( fun_dirs, fun_offs, bnd_dirs, bnd_offs );
+    pa.normalize();
+    P( pa );
 
-//     // VtkOutput va;
-//     // pa.display_vtk( va );
-//     // va.save( "pa.vtk" );
+    // VtkOutput va;
+    // pa.display_vtk( va );
+    // va.save( "pa.vtk" );
 
-//     // pa.normalize();
-//     // P( pa );
+    // pa.normalize();
+    // P( pa );
 
-//     PolyCon<Scalar,nb_dims> pb = pa.legendre_transform();
-//     // pb.normalize();
-//     P( pb );
+    PolyCon<Scalar,nb_dims> pb = pa.legendre_transform();
+    // pb.normalize();
+    P( pb );
 
-//     // VtkOutput vb;
-//     // pb.display_vtk( vb );
-//     // vb.save( "pb.vtk" );
+    // VtkOutput vb;
+    // pb.display_vtk( vb );
+    // vb.save( "pb.vtk" );
 
-//     PolyCon<Scalar,nb_dims> pc = pb.legendre_transform();
-//     pc.normalize();
-//     P( pc );
+    PolyCon<Scalar,nb_dims> pc = pb.legendre_transform();
+    P( pc );
+    pc.normalize();
+    P( pc );
 
-//     // VtkOutput vc;
-//     // pc.display_vtk( vc );
-//     // vc.save( "pc.vtk" );
-// }
+    // VtkOutput vc;
+    // pc.display_vtk( vc );
+    // vc.save( "pc.vtk" );
+}
 
 // TEST_CASE( "PolyCon 1D", "" ) {
 //     constexpr int nb_dims = 1;
@@ -149,50 +150,50 @@
 //     // PolyCon<Scalar,nb_dims> pb = pa.legendre_transform();
 // }
 
-TEST_CASE( "PolyCon 3D spec", "" ) {
-    constexpr int nb_dims = 3;
-    using Scalar = double;
-    using Point = Vec<Scalar,nb_dims>;
+// TEST_CASE( "PolyCon 3D spec", "" ) {
+//     constexpr int nb_dims = 3;
+//     using Scalar = double;
+//     using Point = Vec<Scalar,nb_dims>;
 
-    // Affine functions:
-    //   -0.1641791044780048  +0.0597014925369581  +0.2025586353938458  -0.1582089552243079
-    //   +5.0000000000000000  -5.7500000000007949  +6.7499999999998854  +4.4249999999999199
-    //   +5.0000000000000000  +8.6666666666669698  +8.8095238095238528  +5.8666666666666965
-    // Boundaries:
-    //   -0.9949366763261821  -0.0710669054518706  -0.0710669054518697  -0.0497468338163088
-    //   +0.0000000000000000  -0.7071067811865476  +0.7071067811865476  +0.4949747468305832
-    //   +0.0000000000000000  -0.0000000000000005  -1.0000000000000000  +0.0000000000000000
-    //   +0.0000000000000000  +0.7071067811865476  +0.7071067811865476  +0.4949747468305832
+//     // Affine functions:
+//     //   -0.1641791044780048  +0.0597014925369581  +0.2025586353938458  -0.1582089552243079
+//     //   +5.0000000000000000  -5.7500000000007949  +6.7499999999998854  +4.4249999999999199
+//     //   +5.0000000000000000  +8.6666666666669698  +8.8095238095238528  +5.8666666666666965
+//     // Boundaries:
+//     //   -0.9949366763261821  -0.0710669054518706  -0.0710669054518697  -0.0497468338163088
+//     //   +0.0000000000000000  -0.7071067811865476  +0.7071067811865476  +0.4949747468305832
+//     //   +0.0000000000000000  -0.0000000000000005  -1.0000000000000000  +0.0000000000000000
+//     //   +0.0000000000000000  +0.7071067811865476  +0.7071067811865476  +0.4949747468305832
 
-    Vec<Point> fun_dirs{ 
-        { -0.1641791044776095, +0.0597014925373079, +0.2025586353944587 },
-        { +5.0000000000000000, -5.7500000000000027, +6.7500000000000009 },
-        { +5.0000000000000000, +8.6666666666666625, +8.8095238095238084 },
-    };
-    Vec<Scalar> fun_offs{
-        -0.1582089552238816,
-        +4.4249999999999998,
-        +5.8666666666666654
-    };
-    Vec<Point> bnd_dirs{ 
-        { -0.9949366763261820, -0.0710669054518710, -0.0710669054518700 },
-        { +0.0000000000000000, -0.7071067811865477, +0.7071067811865474 },
-        { +0.0000000000000000, +0.0000000000000000, -1.0000000000000000 },
-        { +0.0000000000000000, +0.7071067811865476, +0.7071067811865476 },
-     };
-    Vec<Scalar> bnd_offs{
-        -0.0497468338163090,
-        +0.4949747468305832,
-        +0.0000000000000004,
-        +0.4949747468305833, 
-    };
+//     Vec<Point> fun_dirs{
+//         { -0.1641791044776095, +0.0597014925373079, +0.2025586353944587 },
+//         { +5.0000000000000000, -5.7500000000000027, +6.7500000000000009 },
+//         { +5.0000000000000000, +8.6666666666666625, +8.8095238095238084 },
+//     };
+//     Vec<Scalar> fun_offs{
+//         -0.1582089552238816,
+//         +4.4249999999999998,
+//         +5.8666666666666654
+//     };
+//     Vec<Point> bnd_dirs{
+//         { -0.9949366763261820, -0.0710669054518710, -0.0710669054518700 },
+//         { +0.0000000000000000, -0.7071067811865477, +0.7071067811865474 },
+//         { +0.0000000000000000, +0.0000000000000000, -1.0000000000000000 },
+//         { +0.0000000000000000, +0.7071067811865476, +0.7071067811865476 },
+//      };
+//     Vec<Scalar> bnd_offs{
+//         -0.0497468338163090,
+//         +0.4949747468305832,
+//         +0.0000000000000004,
+//         +0.4949747468305833,
+//     };
 
-    PolyCon<Scalar,nb_dims> pa( fun_dirs, fun_offs, bnd_dirs, bnd_offs );
-    P( pa );
+//     PolyCon<Scalar,nb_dims> pa( fun_dirs, fun_offs, bnd_dirs, bnd_offs );
+//     P( pa );
 
-    VtkOutput va;
-    pa.display_vtk( va );
-    va.save( "pa.vtk" );
+//     VtkOutput va;
+//     pa.display_vtk( va );
+//     va.save( "pa.vtk" );
 
-    P( pa.legendre_transform() );
-}
+//     P( pa.legendre_transform() );
+// }
